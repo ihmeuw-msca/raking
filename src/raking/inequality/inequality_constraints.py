correct computation of dC/dy

src/raking/inequality/inequality_constraints.py
@@ -112,12 +112,13 @@ def inequality_infant_mortality(
 def inequality_time_trend(
     y: list,
     pop: list
-) -> tuple[np.ndarray, np.ndarray]:
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Compute the constraints matrix C and the inequality vector c for the time trend problem.
 
     We need to define the inequality constraints C beta < c
     when conserving the time trend throught the raking:
-    For years i and i+1 we have (beta_i - beta_i+1) (y_i - y_i+1) >= 0.
+    For years i and i+1 we have (beta_i/pop_i - beta_i+1/pop_i+1) (y_i/pop_i - y_i+1/pop_i+1) >= 0.
+    We also compute the partial derivatives of C with respect to y.
 
     Parameters
     ----------
@@ -132,6 +133,8 @@ def inequality_time_trend(
         (n-1)p * np inequality constraints matrix
     c : np.ndarray
         length np inequality constraints vector
+    DyC : np.ndarray
+        (n-1)p * np * np derivatives of inequality constraints with respect to y
     """
     assert isinstance(y, list), \
         'The observations for all the years must be entered as a list.'
@@ -150,11 +153,20 @@ def inequality_time_trend(
             str(i + 1) + \
             ' must have the same length as the observations for year 1.'
     C = np.zeros((p * (n - 1), p * n))
+    DyC = np.zeros((p * (n - 1), p * n, p * n))
     for i in range(0, n - 1):
         C[(i * p):((i + 1) * p), (i * p):((i + 1) * p)] = \
             np.diag((y[i + 1] / pop[i + 1] - y[i] / pop[i]) / pop[i])
         C[(i * p):((i + 1) * p), ((i + 1) * p):((i + 2) * p)] = \
-            np.diag((y[i] / pop[i] - y[i + 1] / pop[i +1]) / pop[i + 1])
+            np.diag((y[i] / pop[i] - y[i + 1] / pop[i + 1]) / pop[i + 1])
+        DyC[(i * p):((i + 1) * p), (i * p):((i + 1) * p), (i * p):((i + 1) * p)] = \
+            np.fill_diagonal(np.zeros((p, p, p)), - 1.0 / np.square(pop[i]))
+        DyC[(i * p):((i + 1) * p), ((i + 1) * p):((i + 2) * p), (i * p):((i + 1) * p)] = \
+            np.fill_diagonal(np.zeros((p, p, p)), 1.0 / (pop[i] * pop[i + 1]))
+        DyC[(i * p):((i + 1) * p), (i * p):((i + 1) * p), ((i + 1) * p):((i + 2) * p)] = \
+            np.fill_diagonal(np.zeros((p, p, p)), 1.0 / (pop[i] * pop[i + 1]))
+        DyC[(i * p):((i + 1) * p), ((i + 1) * p):((i + 2) * p), ((i + 1) * p):((i + 2) * p)] = \
+            np.fill_diagonal(np.zeros((p, p, p)), - 1.0 / np.square(pop[i + 1]))
     c = np.zeros(p * (n - 1))
-    return (C, c)
+    return (C, c, DyC)
 

src/raking/inequality/loss_functions.py
@@ -98,7 +98,7 @@ def compute_dist(beta, y, q, method, order, l=None, h=None):
             hessian[indices] = (1.0 / q[indices]) * (1.0 / (beta[indices] - l[indices]) + 1.0 / (h[indices] - beta[indices]))
             return hessian
 
-    if order == "both":
+    if order == 'both':
         if method == 'chi2':
             indices = ((q != 0) & (y != 0))
             hessian = np.zeros(len(beta))

src/raking/inequality/set_inequality_problems.py
@@ -116,7 +116,7 @@ def set_time_trend(
     q: list,
     l: list = None,
     h: list = None,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Set up the optimization problem for the time trend problem.
 
     We need to define the problem:
@@ -134,6 +134,10 @@ def set_time_trend(
     s
     C
     c
+    DyC
+    q
+    l
+    h
     """
     n = len(y)
     p = len(y[0])
@@ -142,7 +146,7 @@ def set_time_trend(
     for i in range(0, n):
         A[(i * A0.shape[0]):((i + 1) * A0.shape[0]), (i * A0.shape[1]):((i + 1) * A0.shape[1])] = A0
     s = np.concatenate(s)
-    (C, c) = inequality_time_trend(y, pop)
+    (C, c, DyC) = inequality_time_trend(y, pop)
     y = np.concatenate(y)
     q = np.concatenate(q)
     if l is not None:
@@ -151,5 +155,5 @@ def set_time_trend(
         h = np.concatenate(h)
     else:
         h = None
-    return (y, A, s, C, c, q, l, h)
+    return (y, A, s, C, c, DyC, q, l, h)
 

src/raking/inequality/uncertainty_loss.py
@@ -8,8 +8,10 @@ from raking.inequality.loss_functions import compute_loss, compute_dist
 
 def compute_covariance(
     Dphi_y: np.ndarray,
+    Dphi_c: np.adrray,
     Dphi_s: np.ndarray,
     y_var: np.ndarray,
+    c_var: np.ndarray,
     s_var: np.ndarray,
 ) -> np.ndarray:
     """Compute the covariance matrix of the raked values.
@@ -20,12 +22,16 @@ def compute_covariance(
 
     Parameters
     ----------
-    dPhi_y : np.ndarray
+    Dphi_y : np.ndarray
         Derivatives with respect to the observations
+    Dphi_c : np.ndarray
+        Derivatives with respect to the bounds
     Dphi_s : np.ndarray
         Derivatives with respect to the margins
     y_var : np.ndarray
         Variances of the observations
+    c_var : np.ndarray
+        Variances of the bounds
     s_var : np.ndarray
         Variances of the margins
 
@@ -35,8 +41,8 @@ def compute_covariance(
         Covariance matrix of the raked values
     """
 
-    Dphi = np.concatenate((Dphi_y, Dphi_s), axis=1)
-    sigma = np.concatenate((y_var, s_var))
+    Dphi = np.concatenate((Dphi_y, Dphi_c, Dphi_s), axis=1)
+    sigma = np.concatenate((y_var, c_var, s_var))
     covariance = np.matmul(Dphi * sigma, np.transpose(Dphi))
     return covariance
     
@@ -48,6 +54,7 @@ def compute_gradient(
     s: np.ndarray,
     C: np.ndarray,
     c: np.ndarray,
+    DyC: np.ndarray,
     q: np.ndarray,
     method: str = 'chi2',
     loss: str = 'logit',
@@ -74,28 +81,33 @@ def compute_gradient(
 
     # Gradient with respect to y and s
     dist_both = compute_dist(beta_0, y, q, method, 'both', l, h)
-    DF1_y = np.diag(dist_both)
+    loss_gradient = compute_loss(c - np.matmul(C, beta_0), penalty, loss, 'gradient')
+    DF1_y = np.diag(dist_both) - \
+        np.transpose(np.transpose(DyC, (0, 2, 1)) * loss_gradient, (2, 0, 1)).sum(axis=0) + \
+        np.matmul(np.transpose * loss_hessian, \
+            np.transpose(np.transpose(DyC, (0, 2, 1)) * beta_0, (0, 2, 1)).sum(axis=1))
+    DF1_c = - np.transpose(C) * loss_hessian
     DF1_s = np.zeros((np.shape(A)[1], np.shape(A)[0]))
     DF2_y = np.zeros((np.shape(A)[0], np.shape(A)[1]))
-    DF2_s = -np.identity(np.shape(A)[0])
-    DF_y_s = np.concatenate(
+    DF2_c = np.zeros(((np.shape(C)[1], np.shape(C)[0]))
+    DF2_s = - np.identity(np.shape(A)[0])
+    DF_y_c_s = np.concatenate(
         (
-            np.concatenate((DF1_y, DF1_s), axis=1),
-            np.concatenate((DF2_y, DF2_s), axis=1),
+            np.concatenate((DF1_y, DF1_c, DF1_s), axis=1),
+            np.concatenate((DF2_y, DF2_c, DF2_s), axis=1),
         ),
         axis=0,
     )
 
     # Solve system DF_beta_lambda Dphi_y_s = - DF_y_s
-    Dphi_y_s = np.zeros_like(DF_y_s)
+    Dphi_y_c_s = np.zeros_like(DF_y_c_s)
     lu, piv = lu_factor(DF_beta_lambda)
-    for i in range(0, np.shape(DF_y_s)[1]):
-        Dphi_y_s[:, i] = -lu_solve((lu, piv), DF_y_s[:, i])
+    for i in range(0, np.shape(DF_y_c_s)[1]):
+        Dphi_y_c_s[:, i] = -lu_solve((lu, piv), DF_y_c_s[:, i])
 
     # Return gradient of beta and lambda with respect to y and s
-    Dphi_y = Dphi_y_s[0 : np.shape(A)[1], 0 : np.shape(A)[1]]
-    Dphi_s = Dphi_y_s[
-        0 : np.shape(A)[1], np.shape(A)[1] : (np.shape(A)[0] + np.shape(A)[1])
-    ]
-    return (Dphi_y, Dphi_s)
-    
+    Dphi_y = Dphi_y_c_s[0 : np.shape(A)[1], 0 : np.shape(A)[1]]
+    Dphi_c = Dphi_y_c_s[0 : np.shape(A)[1], np.shape(A)[1] : (np.shape(A)[1] + np.shape(C)[0])]
+    Dphi_s = Dphi_y_c_s[0 : np.shape(A)[1], (np.shape(A)[1] + np.shape(C)[0]) : (np.shape(A)[0] + np.shape(A)[1] + np.shape(C)[0])]
+    return (Dphi_y, Dphi_c, Dphi_s)
+