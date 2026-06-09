30a7e1d582138667e9048e76a85881ddced2df28
ADucellierIHME <ducela@uw.edu>
2026-06-09 09:43:23 -0700

    solver for inequality and penalty

--- src/raking/inequality/solve_loss.py
+++ src/raking/inequality/solve_loss.py
@@ -1,4 +1,4 @@
-"""Solve the raking problem with inequality constraints using an interior point method."""
+"""Solve the raking problem with penalty loss using an interior point method."""
 
 import numpy as np
 import scipy.sparse as sps
@@ -6,6 +6,7 @@ import scipy.sparse as sps
 from scipy.sparse.linalg import cg
 
 from raking.inequality.compute_distance import get_gradient, get_conjugate_gradient, get_conjugate_hessian
+from raking.inequality.compute_loss import get_conjugate_loss_gradient, get_conjugate_loss_hessian
 from raking.raking_methods import raking_chi2, raking_entropic, raking_logit
 
 def centering_step(
@@ -19,6 +20,8 @@ def centering_step(
     lambda0: np.ndarray,
     nu0: np.ndarray,
     method: str = 'chi2',
+    loss: str = 'logit',
+    penalty: float = 1.0,
     l: np.ndarray | None = None,
     h: np.ndarray | None = None,
     epsilon: float = 1.0e-11,
@@ -27,48 +30,80 @@ def centering_step(
     num_iter: int = 100
 ) -> tuple[np.ndarray, float, int]:
     """
-    Implement Newton method to solve min lambda^T c + nu^T s + f(−C^T lambda − A^T nu ; y) − mu 1^T log \lambda.
+    Implement Newton method to solve min lambda^T c + nu^T s + f(−C^T lambda − A^T nu ; y)
+    + L(lambda) - mu 1^T log(-lambda) − mu 1^T log(lambda + penalty).
     """
     n = 0
     x = np.concatenate((lambda0, nu0), axis=0)
     lambda_ = x[0:len(lambda0)]
-    unit = np.concatenate((1.0 / lambda0, np.zeros(len(nu0))), axis=0)
+    unit = np.concatenate((- 1.0 / lambda0, np.zeros(len(nu0))), axis=0)
+    if loss == 'logit':
+        unit_penalty = np.concatenate((1.0 / (lambda0 + penalty), np.zeros(len(nu0))), axis=0)
     b = np.concatenate((c, s), axis=0)
     B = sps.vstack([C, A])
     z = - B.transpose() @ x
     gradient = get_conjugate_gradient(z, y, q, method, l, h)
-    F = b - B @ gradient - mu * unit
+    gradient_loss = get_conjugate_loss_gradient(lambda0, penalty, loss)
+    gradient_loss = np.concatenate((gradient_loss, np.zeros(len(nu0))), axis=0)
+    F = b - B @ gradient + gradient_loss - mu * unit
+    if loss == 'logit':
+        F = F - mu * unit_penalty
     res = np.sqrt(F.transpose() @ F)
     while (res > epsilon) & (n < N):
         hessian = get_conjugate_hessian(z, y, q, method, l, h)
-        unit_square = np.concatenate((1.0 / np.square(lambda_), np.zeros(len(nu0))), axis=0)
-        J = (B.multiply(hessian.diagonal())) @ B.transpose() + mu * unit_square
+        hessian_loss = get_conjugate_loss_hessian(lambda_, penalty, loss)
+        hessian_loss = sps.vstack( \
+            [sps.hstack([hessian_loss, sps.csr_matrix(np.zeros((len(lambda0), len(nu0))))]), \
+            sps.csr_matrix(np.zeros((len(nu0), len(lambda0) + len(nu0))))])
+        unit_square = np.concatenate((- 1.0 / np.square(lambda_), np.zeros(len(nu0))), axis=0)
+        J = (B.multiply(hessian.diagonal())) @ B.transpose() + hessian_loss + mu * unit_square
+        if loss == 'logit':
+            unit_penalty_square = np.concatenate((1.0 / np.square(lambda0 + penalty), np.zeros(len(nu0))), axis=0)
+            J = J + mu * unit_penalty_square
         Delta_x = cg(J, F)[0]
         m = 0
         i = 0
         x_n = x - 2.0 ** (-m) * Delta_x
         lambda_n = x[0:len(lambda0)]
-        unit_n = np.concatenate((1.0 / lambda_n, np.zeros(len(nu0))), axis=0)
+        unit_n = np.concatenate((- 1.0 / lambda_n, np.zeros(len(nu0))), axis=0)
+        if loss == 'logit':
+            unit_penalty_n = np.concatenate((1.0 / (lambda_n + penalty), np.zeros(len(nu0))), axis=0)
         z_n = - B.transpose() @ x_n
         gradient_n = get_conjugate_gradient(z_n, y, q, method, l, h)
-        F_n = b - B @ gradient_n - mu * unit_n
+        gradient_loss_n = get_conjugate_loss_gradient(lambda_n, penalty, loss)
+        gradient_loss_n = np.concatenate((gradient_loss_n, np.zeros(len(nu0))), axis=0)
+        F_n = b - B @ gradient_n + gradient_loss_n - mu * unit_n
+        if loss == 'logit':
+            F_n = F_n - mu * unit_penalty_n
         res_n = np.sqrt(F_n.transpose() @ F_n)
         while (i < num_iter) & ((res_n > (1.0 - gamma_iter * 2.0 ** (-m)) * res) | (np.any(lambda_n < 0))):
             m = m + 1
             x_n = x - 2.0 ** (-m) * Delta_x
             lambda_n = x[0:len(lambda0)]
-            unit_n = np.concatenate((1.0 / lambda_n, np.zeros(len(nu0))), axis=0)
+            unit_n = np.concatenate((- 1.0 / lambda_n, np.zeros(len(nu0))), axis=0)
+            if loss == 'logit':
+                unit_penalty_n = np.concatenate((1.0 / (lambda_n + penalty), np.zeros(len(nu0))), axis=0)
             z_n = - B.transpose() @ x_n
             gradient_n = get_conjugate_gradient(z_n, y, q, method, l, h)
-            F_n = b - B @ gradient_n - mu * unit_n
+            gradient_loss_n = get_conjugate_loss_gradient(lambda_n, penalty, loss)
+            gradient_loss_n = np.concatenate((gradient_loss_n, np.zeros(len(nu0))), axis=0)
+            F_n = b - B @ gradient_n + gradient_loss_n - mu * unit_n
+            if loss == 'logit':
+                F_n = F_n - mu * unit_penalty_n
             res_n = np.sqrt(F_n.transpose() @ F_n)
             i = i + 1
         x = x - 2.0 ** (-m) * Delta_x
         lambda_ = x[0:len(lambda0)]
-        unit = np.concatenate((1.0 / lambda_, np.zeros(len(nu0))), axis=0)
+        unit = np.concatenate((- 1.0 / lambda_, np.zeros(len(nu0))), axis=0)
+        if loss == 'logit':
+            unit_penalty = np.concatenate((1.0 / (lambda_ + penalty), np.zeros(len(nu0))), axis=0)
         z = - B.transpose() @ x
         gradient = get_conjugate_gradient(z, y, q, method, l, h)
-        F = b - B @ gradient - mu * unit
+        gradient_loss = get_conjugate_loss_gradient(lambda_, penalty, loss)
+        gradient_loss = np.concatenate((gradient_loss, np.zeros(len(nu0))), axis=0)
+        F = b - B @ gradient + gradient_loss - mu * unit
+        if loss == 'logit':
+            F = F - mu * unit_penalty
         res = np.sqrt(F.transpose() @ F)
         n = n + 1
     return (x, res, n)
@@ -83,6 +118,8 @@ def barrier_method(
     lambda0: np.ndarray,
     nu0: np.ndarray,
     method: str = 'chi2',
+    loss: str = 'logit',
+    penalty: float = 1.0,
     l: np.ndarray | None = None,
     h: np.ndarray | None = None,
     epsilon: float = 1.0e-11,
@@ -93,13 +130,13 @@ def barrier_method(
     num_iter: int = 100
 ) -> tuple[np.ndarray, float]:
     """
-    Implement barrier method to solve min lambda^T c + nu^T s + f(−C^T lambda − A^T nu ; y) s.t. lambda > 0.
+    Implement barrier method to solve min lambda^T c + nu^T s + f(−C^T lambda − A^T nu ; y) + L(lambda) s.t. lambda in I.
     """
     m = len(lambda0)
     k = len(nu0)
     x = np.concatenate((lambda0, nu0), axis=0)
     while len(x) * mu > epsilon:
-        (x, res, n) = centering_step(y, A, s, C, c, q, mu, lambda0, nu0, method, l, h, epsilon, N, gamma_iter, num_iter)
+        (x, res, n) = centering_step(y, A, s, C, c, q, mu, lambda0, nu0, method, loss, penalty, l, h, epsilon, N, gamma_iter, num_iter)
         lambda0 = x[0:m]
         nu0 = x[m:(m + k)]
         mu = gamma * mu
@@ -113,6 +150,8 @@ def solve(
     c: np.ndarray,
     q: np.ndarray,
     method: str = 'chi2',
+    loss: str = 'logit',
+    penalty: float = 1.0,
     l: np.ndarray | None = None,
     h: np.ndarray | None = None,
     epsilon: float = 1.0e-11,
@@ -137,12 +176,16 @@ def solve(
     m = C.shape[0]
     A = sps.csc_matrix(A)
     C = sps.csc_matrix(C)
+    gradient = get_gradient(beta0, y, q, method, l, h)
     B = sps.vstack([C, A])
-    # Initiate lambda0 with positive value
-    lambda0 = np.ones(m)
+    x0 = cg(B.transpose(), - gradient)[0]
+    lambda0 = x0[0:m]
+    nu0 = x0[m:(m + k)]
     # Run the barrier method and transform back into the primal
-    (x, mu) = barrier_method(y, A, s, C, c, q, lambda0, nu0, method, l, h, epsilon, mu, gamma, N, gamma_iter, num_iter)
+    (x, mu) = barrier_method(y, A, s, C, c, q, lambda0, nu0, method, loss, penalty, l, h, epsilon, mu, gamma, N, gamma_iter, num_iter)
     z = - B.transpose() @ x
     beta = get_conjugate_gradient(z, y, q, method, l, h)
-    return beta
+    lambda_ = x[0:m]
+    zeta = get_conjugate_loss_gradient(lambda_, penalty, loss)
+    return (beta, zeta)
 
